74cc7d005e0d42e9d9547ff9f1c1a773482c4161
bimo.ez <bimo.ez@kakaomobility.com>
2021-11-06 18:04:25 +0900

    Initial Commit

---
+++ TimeTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeTimer
+//
+//  Created by bimo.ez on 2021/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeTimer/TimeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTimerApp.swift
+//  TimeTimer
+//
+//  Created by bimo.ez on 2021/11/06.
+//
+
+import SwiftUI
+
+@main
+struct TimeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
